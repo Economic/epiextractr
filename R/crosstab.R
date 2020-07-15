2336532559f35d8e4ef0425b46bc5535f9a11545
jkandra-epi <jkandra@epi.org>
2020-07-15 17:51:11 -0400

    crosstab row and column summary

--- R/crosstab.R
+++ R/crosstab.R
@@ -9,28 +9,78 @@
 #' @importFrom magrittr %>%
 #' @importFrom rlang :=
 #' @examples crosstab(mtcars, cyl, gear)
-crosstab <- function(df, x, y = NULL, w = NULL) {
+crosstab <- function(df, x, y = NULL, w = NULL, col = FALSE, row = FALSE) {
 
   # need to add row and column percentages
   arg_y <- rlang::enquo(y)
 
-  # one-way tabulation
-  if (rlang::quo_is_null(arg_y)) {
-    df <- df %>%
-      dplyr::count({{ x }}, wt = {{ w }}) %>%
-      dplyr::mutate() %>%
-      dplyr::mutate(freq = n/sum(n), cumul = cumsum(freq))
-  }
-  # two-way tabulation
-  else {
-    df <- df %>%
-      dplyr::count({{ x }}, {{ y }}, wt = {{ w }}) %>%
-      dplyr::mutate("{{ y }}" := labelled::to_character({{ y }})) %>%
-      dplyr::group_by({{ x }}) %>%
-      dplyr::mutate(freq = n/sum(n), cumul = cumsum(freq))
+  # default to counts
+  if (col == FALSE & row == FALSE) {
+    # one-way tabulation
+    if (rlang::quo_is_null(arg_y)) {
+      df <- df %>%
+        dplyr::count({{ x }}, wt = {{ w }}) %>%
+        dplyr::mutate()
+    }
+    # two-way tabulation
+    else {
+      df <- df %>%
+        dplyr::count({{ x }}, {{ y }}, wt = {{ w }}) %>%
+        dplyr::mutate("{{ y }}" := labelled::to_character({{ y }}))
 
+    }
+
+    return(df)
   }
 
-  return(df)
+  # row summary
+  if (col == FALSE & row == TRUE) {
+    # one-way tabulation
+    if (rlang::quo_is_null(arg_y)) {
+      df <- df %>%
+        dplyr::count({{ x }}, wt = {{ w }}) %>%
+        dplyr::mutate() %>%
+        dplyr::mutate(freq = n/sum(n), cumul = cumsum(freq))
+    }
+    # two-way tabulation
+    else {
+      df <- df %>%
+        dplyr::count({{ x }}, {{ y }}, wt = {{ w }}) %>%
+        dplyr::mutate("{{ y }}" := labelled::to_character({{ y }})) %>%
+        dplyr::group_by({{ x }}) %>%
+        dplyr::mutate(freq = n/sum(n), cumul = cumsum(freq))
+
+    }
+
+    return(df)
+  }
+
+  # column summary
+  if (col == TRUE & row == FALSE) {
+    # one-way tabulation
+    if (rlang::quo_is_null(arg_y)) {
+      df <- df %>%
+        dplyr::count({{ x }}, wt = {{ w }}) %>%
+        dplyr::mutate() %>%
+        dplyr::mutate(freq = n/sum(n), cumul = cumsum(freq))
+    }
+    # two-way tabulation
+    else {
+      df <- df %>%
+        dplyr::count({{ x }}, {{ y }}, wt = {{ w }}) %>%
+        dplyr::mutate("{{ y }}" := labelled::to_character({{ y }})) %>%
+        dplyr::group_by({{ y }}) %>%
+        dplyr::mutate(freq = n/sum(n), cumul = cumsum(freq))
+
+    }
+
+    return(df)
+  }
+
+  ## NEED TO FIGURE OUT WHAT ERROR MESSAGE GOES IN HERE
+  if (col == TRUE & row == TRUE) {
+    print(paste("Error"))
+  }
+
 
 }
